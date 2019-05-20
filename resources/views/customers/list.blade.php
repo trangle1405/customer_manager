e1a0569c247597885635605f6dcd01708b93d6f4
Trang Le <trangle1405@gmail.com>
2019-05-20 08:29:28 +0700

    add git commit -m add
    ;

---
+++ resources/views/customers/list.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.layout')
+
+@section('danhsach')
+<div class="col-12">
+    <div class="row">
+        <h1>Danh Sách Khách Hàng</h1>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Tên khách hàng</th>
+                <th scope="col">Ngày Sinh</th>
+                <th scope="col">Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            @if(count($customers) == 0)
+                <tr>
+                    <td colspan="4">Không có dữ liệu</td>
+                </tr>
+            @else
+                @foreach($customers as $key => $customer)
+                    <tr>
+                        <th scope="row">{{ ++$key }}</th>
+                        <td>{{ $customer['name'] }}</td>
+                        <td>{{ $customer['bod'] }}</td>
+                        <td>{{ $customer['email'] }}</td>
+                    </tr>
+                @endforeach
+            @endif
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
